48bbc52b013c9af689d6fa69dc8471d1af655660
Faruk Gökcegöz <farukgokcegoz34@gmail.com>
2021-06-06 00:04:30 +0300

    Basic App.js dosyası oluştuldu.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { BeatLoader } from "react-spinners";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState(true);
 
   useEffect(() => {
     axios
@@ -17,10 +19,47 @@ const App = () => {
       .catch((err) => console.log(err));
   }, []);
 
-  console.log(coins)
-  return <div className="container">
+  console.log(coins);
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+  return (
+    <div className="container">
       <h3 className="text-center">Kripto Para</h3>
-  </div>;
+      <div className="text-center">
+        {isLoading ? (
+          <BeatLoader color="blue" />
+        ) : (
+          <>
+            <table className="table mx-auto">
+              <thead>
+                <tr>
+                  <td className="p-4">Image</td>
+                  <td className="p-4">Name</td>
+                  <td className="p-4">Price</td>
+                  <td className="p-4">24H % Change</td>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  filteredCoins.map((coin)=>{
+                    return(
+                      <Coin key={coin.id}
+                      name= {coin.name}
+                      image={coin.image}
+                      symbol={coin.symbol}
+                      price={coin.current_price}
+                      />
+                    )
+                  })
+                }
+              </tbody>
+            </table>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default App;
