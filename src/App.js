b5fdc9fe418c778aeb6f12de801f0251fe828560
Faruk Gökcegöz <farukgokcegoz34@gmail.com>
2021-06-05 23:42:04 +0300

    Api bağlantısı kuruldu.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
-}
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(coins)
+  return <div className="container">
+      <h3 className="text-center">Kripto Para</h3>
+  </div>;
+};
 
 export default App;
